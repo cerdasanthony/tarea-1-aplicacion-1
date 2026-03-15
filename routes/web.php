d9a333c723169a28514025f1c4fd0f4d9bfa272b
unknown <anthony.cerdas.chacon@est.una.ac.cr>
2026-03-15 17:26:47 -0600

    primera-parte

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return view('main');
+});
+
+Route::get('/books', function () {
+    return view('books');
+});
+
+Route::get('/authors', function () {
+    return view('authors');
+});
+
+Route::get('/publishers', function () {
+    return view('publishers');
+});
